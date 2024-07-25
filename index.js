9fa377c9fe76568451a4be9aa2478f25543bf607
Rushikesh Acharya <rushikesha@google.com>
2024-07-25 22:24:21 +0530

    feat: query params

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const http = require("http");
 const fs = require("fs");
+const url = require("url");
 
 const server = http
   .createServer((req, res) => {
     const log = `\n ${Date.now()} : ${req.url} New Request Received...`;
+    if(req.url === "/favicon.ico") return res.end();
+    const myUrl = url.parse(req.url,true);
+    console.log("URL", myUrl);
     fs.appendFile("log.txt", log, (err, data) => {
-      switch (req.url) {
+      switch (myUrl.pathname) {
         case "/":
           res.end("Home Page");
           break;
